1c9a850034fdcab20ac7448f341eaf70dc2b3303
Luís Fernando <luisfernando.25@hotmail.com>
2021-05-10 20:54:02 -0300

    Criar metodo de pegar pais por id

--- src/main/java/com/digitalinnovation/citiesapi/countries/resource/CountryResource.java
+++ src/main/java/com/digitalinnovation/citiesapi/countries/resource/CountryResource.java
@@ -5,11 +5,13 @@ import com.digitalinnovation.citiesapi.countries.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -23,4 +25,12 @@ public class CountryResource {
         return repository.findAll(page);
     }
 
+    @GetMapping("/{id}")
+    private ResponseEntity getCountry(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+        if(optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
